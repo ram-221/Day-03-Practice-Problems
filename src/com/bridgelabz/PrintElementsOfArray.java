50d3279e81e069b25f20f5a5e03ef5b4f222ab52
ram-221 <rameshmalli857.com>
2022-10-24 22:05:16 +0530

    print the elements of an array

---
+++ src/com/bridgelabz/PrintElementsOfArray.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+public class PrintElementsOfArray {
+
+	public static void main(String[] args) {
+		
+		int a[] =new int[] {12,24,36,48,60,72,84};
+		for(int x=0;x<a.length;x++) {
+			System.out.print(a[x] +" ");
+		}
+		System.out.println("this are elements of aray");
+
+	}
+
+}
