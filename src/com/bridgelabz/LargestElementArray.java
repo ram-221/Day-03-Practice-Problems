71194746d45695d7e9cfe090ff8cf51456625b21
ram-221 <rameshmalli857.com>
2022-10-24 22:07:48 +0530

    print the largest element in an array

---
+++ src/com/bridgelabz/LargestElementArray.java
@@ -0,0 +1,17 @@
+package com.bridgelabz;
+
+public class LargestElementArray {
+	
+	public static void main(String arg[]) {
+		
+		int array[] = new int[] {5,12,45,9,356};
+		int largest = array[0];
+		for (int index=0; index<array.length;index++) {
+			if(array[index]>largest) 
+				largest=array[index];
+		}
+			System.out.println("Largest element of array: "+largest);
+		
+	}
+
+}
